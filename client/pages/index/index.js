42bbad7bedde70c628c36dbc9a5bbcd711f76239
Sonia:)fy <931863235@qq.com>
2021-11-04 16:55:34 +0800

    this is a new project

---
+++ client/pages/index/index.js
@@ -0,0 +1,43 @@
+import { invokeFunction } from '../../utils/serverless'
+
+Page({
+  async onLoad(query) {
+    // 查询订单
+    await invokeFunction({
+      name: 'main',
+      url: 'order/query',
+      data: {
+        orderId: 1
+      }
+    })
+  },
+  onReady() {
+    // 页面加载完成
+  },
+  onShow() {
+    // 页面显示
+  },
+  onHide() {
+    // 页面隐藏
+  },
+  onUnload() {
+    // 页面被关闭
+  },
+  onTitleClick() {
+    // 标题被点击
+  },
+  onPullDownRefresh() {
+    // 页面被下拉
+  },
+  onReachBottom() {
+    // 页面被拉到底部
+  },
+  onShareAppMessage() {
+    // 返回自定义分享信息
+    return {
+      title: 'My App',
+      desc: 'My App description',
+      path: 'pages/index/index',
+    };
+  },
+});
